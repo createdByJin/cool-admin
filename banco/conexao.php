1a7be84346d56b6bf22782d18e49df5eb7563ee9
Danilo Fontes de Carvalho <danilo_f_de-carvalho@estudante.sc.senai.br>
2021-07-22 22:03:13 -0300

    deletando páginas

--- banco/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    //Abrir conexão com o Banco de Dados
-    //$conn = mysqli_connect("servidor","usuario","senha","banco") or die("Falha: " . mysqli_connect_error());
-    $conn = mysqli_connect("localhost","root","","drocsid-preenchido") or die("Falha: " . mysqli_connect_error())
-
-?>
